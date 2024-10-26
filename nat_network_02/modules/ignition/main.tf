e96d7610be14332ca6067fdab0a65fed02464e28
Victor Galvez <vhgalvez@gmail.com>
2024-10-26 22:47:22 +0200

    Update main.tf

--- nat_network_02/modules/ignition/main.tf
+++ nat_network_02/modules/ignition/main.tf
@@ -1,17 +1,17 @@
 resource "libvirt_ignition" "bootstrap_ignition" {
   name    = "okd_bootstrap.ign"
   pool    = "default"
-  content = "/home/victory/openshift_okd_cluster/terraform/ignition_configs/bootstrap.ign"
+  content = "/home/victory/okd_cluster_openshift_fedora_coreos_kvm/nat_network_02/ignition_configs/bootstrap.ign"
 }
 
 resource "libvirt_ignition" "master_ignition" {
   name    = "okd_master.ign"
   pool    = "default"
-  content = "/home/victory/openshift_okd_cluster/terraform/ignition_configs/master.ign"
+  content = "/home/victory/okd_cluster_openshift_fedora_coreos_kvm/nat_network_02/ignition_configs/master.ign"
 }
 
 resource "libvirt_ignition" "master_ignition" {
   name    = "okd_worker.ign"
   pool    = "default"
-  content = "/home/victory/openshift_okd_cluster/terraform/ignition_configs/worker.ign"
+  content = "//home/victory/okd_cluster_openshift_fedora_coreos_kvm/nat_network_02/ignition_configs/worker.ign"
 }
