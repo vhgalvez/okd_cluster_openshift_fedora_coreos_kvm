b9bb28685f1ee18c4426efc524917c7ed10948cc
Victor Galvez <vhgalvez@gmail.com>
2024-10-26 23:08:44 +0200

    Update main.tf

--- nat_network_02/modules/ignition/main.tf
+++ nat_network_02/modules/ignition/main.tf
@@ -1,17 +1,22 @@
+# nat_network_02\modules\ignition\main.tf
+
+# Definición del recurso para el archivo Ignition del bootstrap
 resource "libvirt_ignition" "bootstrap_ignition" {
   name    = "okd_bootstrap.ign"
   pool    = "default"
   content = "/home/victory/okd_cluster_openshift_fedora_coreos_kvm/nat_network_02/ignition_configs/bootstrap.ign"
 }
 
+# Definición del recurso para el archivo Ignition del master
 resource "libvirt_ignition" "master_ignition" {
   name    = "okd_master.ign"
   pool    = "default"
   content = "/home/victory/okd_cluster_openshift_fedora_coreos_kvm/nat_network_02/ignition_configs/master.ign"
 }
 
+# Definición del recurso para el archivo Ignition de los workers
 resource "libvirt_ignition" "worker_ignition" {
   name    = "okd_worker.ign"
   pool    = "default"
-  content = "//home/victory/okd_cluster_openshift_fedora_coreos_kvm/nat_network_02/ignition_configs/worker.ign"
+  content = "/home/victory/okd_cluster_openshift_fedora_coreos_kvm/nat_network_02/ignition_configs/worker.ign"
 }
